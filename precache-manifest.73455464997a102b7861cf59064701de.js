e28134c15470298644ed9046133ba9429eff4bf1
siddharth <robertjackman58@gmail.com>
2021-02-22 18:01:56 +0530

    Updates

---
+++ precache-manifest.73455464997a102b7861cf59064701de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa32175f8fadf20371f8",
+    "url": "/robofriends/static/css/main.f6e08349.chunk.css"
+  },
+  {
+    "revision": "fa32175f8fadf20371f8",
+    "url": "/robofriends/static/js/main.303db9ba.chunk.js"
+  },
+  {
+    "revision": "6ef892fa29c515528340",
+    "url": "/robofriends/static/js/runtime~main.53826fbb.js"
+  },
+  {
+    "revision": "8c632611e3c2a6372ab2",
+    "url": "/robofriends/static/css/2.4041c67f.chunk.css"
+  },
+  {
+    "revision": "8c632611e3c2a6372ab2",
+    "url": "/robofriends/static/js/2.971b8cad.chunk.js"
+  },
+  {
+    "revision": "0b270de85f9ad9ac6688",
+    "url": "/robofriends/static/js/3.86d2ab6d.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "12545f40d7548c0d1af73c81c46bc768",
+    "url": "/robofriends/index.html"
+  }
+];
